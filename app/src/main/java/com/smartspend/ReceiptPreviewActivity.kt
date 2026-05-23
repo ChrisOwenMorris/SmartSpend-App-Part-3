4025835a0116e13f4bf7b70b3570bff2830740c0
ST10266958 <st10266958@vcconnect.edu.za>
2026-05-23 15:31:11 +0200

    Receipt Functionality updated

---
+++ app/src/main/java/com/smartspend/ReceiptPreviewActivity.kt
@@ -0,0 +1,26 @@
+package com.smartspend
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+class ReceiptPreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receipt_preview)
+
+        val imageView = findViewById<ImageView>(R.id.fullReceiptImage)
+        val imagePath = intent.getStringExtra("receiptPath")
+
+        if (!imagePath.isNullOrEmpty()) {
+            val file = File(imagePath)
+            if (file.exists()) {
+
+                imageView.setImageURI(Uri.fromFile(file))
+            }
+        }
+    }
+}
